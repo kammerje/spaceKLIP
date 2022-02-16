dcf0f351bc3f4812c8d79bdbeb8f93b2db9081e0
Aarynn Carter <23636747+AarynnCarter@users.noreply.github.com>
2022-02-15 16:51:50 -0800

    Packaging things up, attempt #1

--- run_pipeline.py
+++ run_pipeline.py
@@ -1,6 +1,3 @@
-import sys
-sys.path.append('..')
-
 import pyKLIP_for_Webb
 from pyKLIP_for_Webb.engine import JWSTPipeline
 
---
+++ setup.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+
+with open('requirements.txt') as f:
+    REQUIRES = f.read().splitlines()
+
+
+setup(name='pyKLIP_for_Webb',
+      version=0.0.1,
+      description='Reduction pipeline for JWST Coronagraphy',
+      author='ERS-1386 Collaboration',
+      author_email='aarynn.carter@gmail.com',
+      license='MIT',
+      install_requires=REQUIRES,
+      packages=find_packages()
+    )
