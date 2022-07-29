ac3675d131a0c4954df0bc79511239f47b882700
Aarynn Carter <23636747+AarynnCarter@users.noreply.github.com>
2022-07-29 14:33:19 -0700

    Adding DQ median correction

--- tests/run_pipeline.py
+++ tests/run_pipeline.py
@@ -7,8 +7,8 @@ config_file = os.path.dirname(__file__)+'/example_config.yaml'
 if __name__ == '__main__':
 	pipe = JWST(config_file)
 	pipe.run_all(skip_ramp=True, 
-				 skip_imgproc=True, 
-				 skip_sub=False, 
+				 skip_imgproc=False, 
+				 skip_sub=True, 
 				 skip_rawcon=True, 
 				 skip_calcon=True, 
 				 skip_comps=True)
