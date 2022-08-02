60ebcb08ae245364bfd6977e0500fe08561b2b5e
Aarynn Carter <23636747+AarynnCarter@users.noreply.github.com>
2022-08-02 10:30:24 -0700

    Switch between call or run using meta.call_or_run

--- spaceKLIP/rampfit.py
+++ spaceKLIP/rampfit.py
@@ -15,34 +15,50 @@ def run_ramp_fitting(meta, idir, osubdir):
 	for file in files:
 		# Set up pipeline
 		pipeline = Detector1Pipeline()
-
-		steps = pipeline.steps
+		psteps = pipeline.steps
 
 		# If we're forcing reference pixels need to skip a lot of steps
 		# that get performed elsewhere
 		if meta.force_ref_pixels != False:
 			file = preprocess_file(meta, file, osubdir)
-			steps['group_scale']['skip'] = True
-			steps['dq_init']['skip'] = True
-			steps['saturation']['skip'] = True
-			steps['superbias']['skip'] = True
-			steps['refpix']['skip'] = True
-
-		steps['saturation']['n_pix_grow_sat']=meta.saturation_boundary
-		steps['jump']['skip'] = meta.skip_jump
-		steps['jump']['rejection_threshold'] = meta.jump_threshold
-		steps['jump']['maximum_cores'] = meta.ramp_fit_max_cores
-		steps['dark_current']['skip'] = meta.skip_dark_current
-		steps['ipc']['skip'] = meta.skip_ipc
+			if meta.call_or_run == 'call':
+				psteps['group_scale']['skip'] = True
+				psteps['dq_init']['skip'] = True
+				psteps['saturation']['skip'] = True
+				psteps['superbias']['skip'] = True
+				psteps['refpix']['skip'] = True
+			else:
+				pipeline.group_scale.skip = True
+				pipeline.dq_init.skip = True
+				pipeline.saturation.skip = True
+				pipeline.superbias.skip = True
+				pipeline.refpix.skip = True
 
 		# Set up directory to save into
 		pipeline.output_dir = meta.odir + osubdir
 		if os.path.exists(pipeline.output_dir) == False:
 			os.makedirs(pipeline.output_dir)
-		
-		# Call pipeline
-		pipeline.call(file, output_dir=pipeline.output_dir, steps=pipeline.steps, \
-						save_results=True)
+
+		if meta.call_or_run == 'call':
+			# Call pipeline
+			psteps['saturation']['n_pix_grow_sat']=meta.saturation_boundary
+			psteps['jump']['skip'] = meta.skip_jump
+			psteps['jump']['rejection_threshold'] = meta.jump_threshold
+			psteps['jump']['maximum_cores'] = meta.ramp_fit_max_cores
+			psteps['dark_current']['skip'] = meta.skip_dark_current
+			psteps['ipc']['skip'] = meta.skip_ipc
+			pipeline.call(file, output_dir=pipeline.output_dir, steps=psteps, \
+		 				save_results=True)
+		else: 
+			# Run pipeline
+			pipeline.saturation.n_pix_grow_sat = meta.saturation_boundary
+			pipeline.jump.skip = meta.skip_jump
+			pipeline.jump.rejection_threshold = meta.jump_threshold
+			pipeline.jump.maximum_cores = meta.ramp_fit_max_cores
+			pipeline.dark_current.skip = meta.skip_dark_current
+			pipeline.ipc.skip = meta.skip_ipc
+			pipeline.save_results = True
+			pipeline.run(file)
 
 	return
 
@@ -65,20 +81,36 @@ def preprocess_file(meta, file, osubdir):
 	# Need to run things in steps, first skip everything after superbias
 	pipe = Detector1Pipeline()
 
-	steps = pipe.steps
-	steps['refpix']['skip'] = True
-	steps['saturation']['skip'] = True
-	steps['linearity']['skip'] = True
-	steps['persistence']['skip'] = True
-	steps['dark_current']['skip'] = True
-	steps['jump']['skip'] = True
-	steps['ramp_fit']['skip'] = True
-	steps['gain_scale']['skip'] = True
-	steps['ipc']['skip'] = meta.skip_ipc
-	steps['saturation']['n_pix_grow_sat'] = meta.saturation_boundary
+	if meta.call_or_run == 'call':
+		# Call pipeline
+		steps = pipe.steps
+		steps['refpix']['skip'] = True
+		steps['saturation']['skip'] = True
+		steps['linearity']['skip'] = True
+		steps['persistence']['skip'] = True
+		steps['dark_current']['skip'] = True
+		steps['jump']['skip'] = True
+		steps['ramp_fit']['skip'] = True
+		steps['gain_scale']['skip'] = True
+		steps['ipc']['skip'] = meta.skip_ipc
+		steps['saturation']['n_pix_grow_sat'] = meta.saturation_boundary
 
-	#Run pipeline
-	result = pipe.call(file, steps=pipe.steps)
+		result = pipe.call(file, steps=steps)
+	else:
+		# Run pipeline
+		pipe.refpix.skip = True
+		pipe.linearity.skip = True
+		pipe.saturation.skip = True
+		pipe.persistence.skip = True
+		pipe.dark_current.skip = True
+		pipe.jump.skip = True
+		pipe.ramp_fit.skip = True
+		pipe.gain_scale.skip = True
+		pipe.ipc.skip = meta.skip_ipc
+		pipe.saturation.n_pix_grow_sat =  meta.saturation_boundary
+		pipe.save_results = False
+
+		result = pipe.run(file)
 
 	# Redefine some of the reference pixels
 	val = int(meta.force_ref_pixels)
@@ -87,7 +119,8 @@ def preprocess_file(meta, file, osubdir):
 
 	# Run reference pixel correction
 	refpix_step_amp_corr = RefPixStep(use_side_ref_pixels=False)
-	refpix_amp_corr = refpix_step_amp_corr.run(result)
+	refpix_step_amp_corr.save_results = False
+	refpix_amp_corr = refpix_step_amp_corr.run(result) # Okay to use run() here
 
 	# Restore pixels
 	refpix_amp_corr.pixeldq[0:val,:] = refpix_amp_corr.pixeldq[0:val,:] & ~dqflags.pixel['REFERENCE_PIXEL']
--- tests/run_pipeline.py
+++ tests/run_pipeline.py
@@ -8,8 +8,8 @@ config_file = os.path.dirname(__file__)+'/nircam_config.yaml'
 if __name__ == '__main__':
 	pipe = JWST(config_file)
 	pipe.run_all(skip_ramp=False, 
-				 skip_imgproc=False, 
-				 skip_sub=False, 
+				 skip_imgproc=True, 
+				 skip_sub=True, 
 				 skip_rawcon=True, 
 				 skip_calcon=True, 
 				 skip_comps=True)
