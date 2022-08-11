c007bbc596e3e0f3343d3ee702b3ff433d10378c
maxwellmb <maxmb@ucsb.edu>
2022-08-10 17:55:49 -0700

    changes to help find star position

--- spaceKLIP/subtraction.py
+++ spaceKLIP/subtraction.py
@@ -429,11 +429,14 @@ def klip_subtraction(meta, files):
                     filepaths = np.array(meta.obs[key]['FITSFILE'][ww_sci], dtype=str).tolist()
                     ww_cal = np.where(meta.obs[key]['TYP'] == 'CAL')[0]
                     psflib_filepaths = np.array(meta.obs[key]['FITSFILE'][ww_cal], dtype=str).tolist()
+                    load_file0_center = meta.load_file0_center if hasattr(meta,'load_file0_center')  else False
+
                     dataset = JWST.JWSTData(filepaths=filepaths,
                                             psflib_filepaths=psflib_filepaths, centering=meta.centering_alg, badpix_threshold=meta.badpix_threshold,
                                             scishiftfile=meta.ancildir+'shifts/scishifts', refshiftfile=meta.ancildir+'shifts/refshifts',
-                                            fiducial_point_override=meta.fiducial_point_override, blur=meta.blur_images)
-                    
+                                            fiducial_point_override=meta.fiducial_point_override, blur=meta.blur_images,
+                                            load_file0_center=load_file0_center,save_center_file=meta.ancildir+'shifts/file0_centers',
+                                            spectral_type=meta.spt)
                     #Set an OWA if it exists. 
                     if hasattr(meta, 'OWA'): dataset.OWA = meta.OWA
 
