047d505e523e10ad9fd3631616173c16b160406c
wbalmer <williamobalmer@gmail.com>
2023-03-17 15:54:15 -0400

    add back planetkiller

--- tests/run_planetkiller.py
+++ tests/run_planetkiller.py
@@ -23,8 +23,8 @@ if __name__ == '__main__':
 	pipe = JWST(config_file)
 	pipe.run_all(skip_ramp=True,
 				 skip_imgproc=True,
-				 skip_sub=True,
+				 skip_sub=False,
 				 skip_rawcon=True,
 				 skip_calcon=True,
-				 skip_comps=True)
+				 skip_comps=False)
 	sklip.companion.planet_killer(pipe.meta, small_planet=small_planet)
