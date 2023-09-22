3e5e11e0cc03092707a8ce8d3e600cd4d6ccf93d
Jarron Leisenring <JarronL@users.noreply.github.com>
2023-09-21 17:03:27 -0700

    Update coron1pipeline.py
    add machinery in coron1pipeline to model and remove 1/f noise and subtract kTC noise from ramps (required for clean residuals to fit 1/f noise)

--- spaceKLIP/coron1pipeline.py
+++ spaceKLIP/coron1pipeline.py
@@ -16,8 +16,13 @@ import astropy.io.fits as pyfits
 import matplotlib.pyplot as plt
 import numpy as np
 
-from jwst.datamodels import dqflags, RampModel
+from tqdm.auto import trange
+
+from jwst.lib import reffile_utils
+from jwst.datamodels import dqflags, RampModel, SaturationModel
 from jwst.pipeline import Detector1Pipeline, Image2Pipeline, Coron3Pipeline
+
+from webbpsf_ext import robust
 
 import logging
 log = logging.getLogger(__name__)
@@ -32,6 +37,21 @@ class Coron1Pipeline_spaceKLIP(Detector1Pipeline):
     """
     The spaceKLIP JWST stage 1 pipeline class.
     
+    Apply all calibration steps to raw JWST ramps to produce 
+    a 2-D slope product. Custom sub-class of ``Detector1Pipeline`` 
+    with modifications for coronagraphic data.
+    
+    Included steps are: group_scale, dq_init, saturation, ipc, 
+    superbias, refpix, rscd, firstframe, lastframe, linearity, 
+    dark_current, reset, persistence, jump detection, ramp_fit, 
+    and gain_scale. 
+    """
+    
+    class_alias = "calwebb_coron1"
+
+    spec = """
+        remove_ktc         = boolean(default=True) # Remove kTC noise from data
+        remove_fnoise      = boolean(default=True) # Remove 1/f noise from data
     """
     
     def __init__(self,
@@ -100,6 +120,7 @@ class Coron1Pipeline_spaceKLIP(Detector1Pipeline):
             input = self.run_step(self.rscd, input)
             input = self.run_step(self.dark_current, input)
             input = self.do_refpix(input)
+            input = self.run_step(self.jump, input)
         else:
             input = self.run_step(self.group_scale, input)
             input = self.run_step(self.dq_init, input)
@@ -110,7 +131,11 @@ class Coron1Pipeline_spaceKLIP(Detector1Pipeline):
             input = self.run_step(self.linearity, input)
             input = self.run_step(self.persistence, input)
             input = self.run_step(self.dark_current, input)
-        input = self.run_step(self.jump, input)
+            input = self.run_step(self.jump, input)
+            if self.remove_ktc or self.remove_fnoise:
+                input = self.subtract_ktc(input)
+            if self.remove_fnoise:
+                input = self.subtract_fnoise(input, model_type='savgol')
         
         # Save calibrated ramp data.
         if self.ramp_fit.save_calibrated_ramp:
@@ -351,6 +376,168 @@ class Coron1Pipeline_spaceKLIP(Detector1Pipeline):
         res.pixeldq[nrow_off:nupper_off, -ncol_off - nright:nright_off] = res.pixeldq[nrow_off:nupper_off, -ncol_off - nright:nright_off] & ~dqflags.pixel['REFERENCE_PIXEL']
         
         return res
+
+    def _fit_slopes(self,
+                    input,
+                    sat_frac=0.5):
+        """Fit slopes to each integration
+        
+        Uses custom `cube_fit` function to fit slopes to each integration.
+        Returns aray of slopes and bias values for each integration.
+        Bias and slope arrays have shape (nints, ny, nx).
+
+        Parameters
+        ----------
+        input : jwst.datamodel
+            Input JWST datamodel housing the data to be fit.
+        sat_frac : float
+            Saturation threshold for fitting. Values above
+            this fraction of the saturation level are ignored.
+            Default is 0.5 to ensure that the fit is within 
+            the linear range.
+        """
+
+        from .imagetools import cube_fit
+
+        # Get saturation reference file
+        # Get the name of the saturation reference file
+        sat_name = self.saturation.get_reference_file(input, 'saturation')
+
+        # Open the reference file data model
+        sat_model = SaturationModel(sat_name)
+
+        # Extract subarray from saturation reference file, if necessary
+        if reffile_utils.ref_matches_sci(input, sat_model):
+            sat_thresh = sat_model.data.copy()
+        else:
+            ref_sub_model = reffile_utils.get_subarray_model(input, sat_model)
+            sat_thresh = ref_sub_model.data.copy()
+            ref_sub_model.close()
+
+        # Close the reference file
+        sat_model.close()
+
+        # Perform ramp fit to data to get bias offset
+        group_time = input.meta.exposure.group_time
+        ngroups = input.meta.exposure.ngroups
+        nints = input.meta.exposure.nints
+        tarr = np.arange(1, ngroups+1) * group_time
+        data = input.data
+
+        bias_arr = []
+        slope_arr = []
+        for i in range(nints):
+            # Get group-level bpmask for this integration
+            groupdq = input.groupdq[i]
+            # Make sure to accumulate the group-level dq mask
+            bpmask_arr = np.cumsum(groupdq, axis=0) > 0
+            cf = cube_fit(tarr, data[i], bpmask_arr=bpmask_arr,
+                          sat_vals=sat_thresh, sat_frac=sat_frac)
+            bias_arr.append(cf[0])
+            slope_arr.append(cf[1])
+        bias_arr = np.asarray(bias_arr)
+        slope_arr = np.asarray(slope_arr)
+
+        # bias and slope arrays have shape [nints, ny, nx]
+        # bias values are in units of DN and slope in DN/sec
+        return bias_arr, slope_arr
+
+    def subtract_ktc(self,
+                     input,
+                     sat_frac=0.5):
+        
+        bias_arr, _ = self._fit_slopes(input, sat_frac=sat_frac)
+
+        # Subtract bias from each integration
+        nints = input.meta.exposure.nints
+        for i in range(nints):
+            input.data[i] -= bias_arr[i]
+
+        return input
+    
+    def subtract_fnoise(self,
+                        input,
+                        sat_frac=0.5,
+                        **kwargs):
+        """Model and subtract 1/f noise from each integration
+        
+        TODO: Make this into a Step class.
+        TODO: Automatic function to determine if correction is necessary.
+
+        Parameters
+        ----------
+        input : jwst.datamodel
+            Input JWST datamodel to be processed.
+
+        Keyword Args
+        ------------
+        model_type : str
+            Must be 'median', 'mean', or 'savgol'. For 'mean' case,
+            it uses a robust mean that ignores outliers and NaNs.
+            The 'median' case uses `np.nanmedian`. The 'savgol' case
+            uses a Savitzky-Golay filter to model the 1/f noise, 
+            iteratively rejecting outliers from the model fit relative
+            to the median model. The default is 'savgol'.
+        """
+        
+        from .fnoise_clean import CleanSubarray
+
+        is_full_frame = 'FULL' in input.meta.subarray.name.upper()
+        nints    = input.meta.exposure.nints
+        ngroups  = input.meta.exposure.ngroups
+        noutputs = input.meta.exposure.noutputs
+
+        if is_full_frame:
+            assert noutputs == 4, 'Full frame data must have 4 outputs'
+        else:
+            assert noutputs == 1, 'Subarray data must have 1 output'
+
+        ny, nx = input.data.shape[-2:]
+        chsize = ny // noutputs
+
+        # Fit slopes to get signal mask
+        # Grab slopes if they've already been computed
+        _, slope_arr = self._fit_slopes(input, sat_frac=sat_frac)
+        slope_mean = robust.mean(slope_arr, axis=0)
+
+        # Generate a mean signal ramp to subtract from each group
+        group_time = input.meta.exposure.group_time
+        ngroups = input.meta.exposure.ngroups
+        tarr = np.arange(1, ngroups+1) * group_time
+        signal_mean_ramp = slope_mean * tarr.reshape([-1,1,1])
+
+        # Subtract 1/f noise from each integration
+        data = input.data
+        for i in trange(nints):
+            cube = data[i]
+            groupdq = input.groupdq[i]
+            # Cumulative sum of group DQ flags
+            bpmask_arr = np.cumsum(groupdq, axis=0) > 0
+            for j in range(ngroups):
+                # Exclude bad pixels
+                im_mask = ~bpmask_arr[j] #& mask
+                for ch in range(noutputs):
+                    # Get channel x-indices
+                    x1 = int(ch*chsize)
+                    x2 = int(x1 + chsize)
+
+                    # Channel subarrays
+                    imch = cube[j, :, x1:x2]
+                    sigch = signal_mean_ramp[j, :, x1:x2]
+                    good_mask = im_mask[:, x1:x2]
+
+                    # Remove averaged signal goup
+                    imch_diff = imch - sigch
+
+                    # Subtract 1/f noise
+                    nf_clean = CleanSubarray(imch_diff, good_mask)
+                    nf_clean.fit(**kwargs)
+                    # Subtract model from data
+                    data[i,j,:,x1:x2] -= nf_clean.model
+
+                    del nf_clean
+
+        return input
 
 def run_obs(database,
             steps={},
