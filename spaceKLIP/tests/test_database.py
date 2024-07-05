fb62e31b322ecca8c79312bbba7ea21bc7264e36
AarynnCarter <23636747+AarynnCarter@users.noreply.github.com>
2024-07-05 10:55:44 -0400

    Attempting to resolve testing issue by defining additional CRDS parameters... now using tox.ini

--- spaceKLIP/tests/test_database.py
+++ spaceKLIP/tests/test_database.py
@@ -9,7 +9,6 @@ testdatapath = os.getenv('SPACEKLIP_TEST_DATA_PATH')
 testpath = os.path.dirname(os.path.abspath(__file__))
 
 os.environ['CRDS_SERVER_URL'] = 'https://jwst-crds.stsci.edu'
-os.makedirs('/grp/crds/cache/config/jwst/server_config')
 os.environ['CRDS_PATH'] = '/grp/crds/cache/'
 
 def test_has_test_data():
